5b74c22f2d455e5ee34b0fce445e38c5d832f31e
Troy Han <919373602@qq.com>
2021-04-08 20:16:21 +0800

    finished signup function

--- bbs/urls.py
+++ bbs/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('index', views.index, name='index'),
-    path('client/<int:client_id>/', views.clientProfile, name='clientProfile'),
-    path('client/signin/', views.clientSignIn, name='clientSignIn'),
-    path('client/signup/', views.clientSignUp, name='clientSignUp'),
+    path('client/<int:client_id>/', views.client_profile, name='clientProfile'),
+    path('client/signin/', views.client_sign_in, name='clientSignIn'),
+    path('client/signup/', views.client_sign_up, name='clientSignUp'),
 ]
--- bbs/views.py
+++ bbs/views.py
@@ -2,21 +2,42 @@ from django.shortcuts import render
 from .models import Client
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import get_object_or_404,render
+from django.core.exceptions import ObjectDoesNotExist
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the bbs index.")
 
-def clientProfile(request, client_id):
-    client = Client.objects.get(id=client_id)
-    return HttpResponse(f"This is {client.name}")
 
-def clientSignIn(request):
+def client_sign_in(request):
     context = {}
-    template = loader.get_template('bbs/client/signin.html')
-    return HttpResponse(template.render(context, request))
+    if request.method == 'POST':
+        try:
+            Client.objects.get(email=request.POST['email'])
+            print('ERROR')
+            context["message"] = "The Email is used. Please select another email."
+        except ObjectDoesNotExist:
+            context["message"] = f"{request.POST['email']} is successfully registered."
+            new_client = Client(
+                email=request.POST['email'],
+                password=request.POST['password'],
+                phone_number=request.POST['phone'],
+                name=request.POST['name'],
+                major=request.POST['major'],
+                grade=request.POST['grade']
+            )
+            new_client.save()
 
-def clientSignUp(request):
+    return render(request, 'bbs/client/signin.html', context)
+
+
+def client_sign_up(request):
     context = {}
     template = loader.get_template('bbs/client/signup.html')
     return HttpResponse(template.render(context, request))
 
+
+def client_profile(request, client_id):
+    client = Client.objects.get(id=client_id)
+    return HttpResponse(f"This is {client.name}")
